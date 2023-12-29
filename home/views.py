4fd23b0c36b234105b682af65588f2606074d9f0
PSebastian96 <>
2023-12-29 15:29:13 +0000

    testing

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 
 def index(request):
-    """ A view to return the index page """
-
-    return render(request, 'home/index.html')
+    """ A view to return the index page with current date """
+    present_year = datetime.now().year
+    return render(request, 'home/index.html', {'present_year': present_year})
