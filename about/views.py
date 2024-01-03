5c48f6a44c5a9f41815f7d097c6cf1a1e17c8996
PSebastian96 <>
2024-01-03 22:14:43 +0000

    save

--- about/views.py
+++ about/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+
+
+def about_us(request):
+    """ A view to return the about us page """
+
+    return render(request, 'about/about_us.html')
