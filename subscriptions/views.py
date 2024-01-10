cdccad42bb0d85da7fa78954e8f6a52ba2b28f0c
PSebastian96 <sebipin96@gmail.com>
2024-01-10 14:02:08 +0000

    restyling

--- subscriptions/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from datetime import datetime
-
-# Create your views here.
-
-def subscriptions(request):
-    """ A view to subscriptions page """
-    present_year = datetime.now().year
-    return render(request, 'subscriptions/subscriptions.html', {'present_year': present_year})
