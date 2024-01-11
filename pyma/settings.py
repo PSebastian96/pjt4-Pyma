578a5fc490384bd5332fdee6208463a671bc8b4e
PSebastian96 <sebipin96@gmail.com>
2024-01-11 20:43:58 +0000

    gmail change

--- pyma/settings.py
+++ pyma/settings.py
@@ -107,8 +107,6 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -214,6 +212,7 @@ STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'pyma@snacks.com'
 
+# Django email
 
 if 'USE_EMAIL_SERVICE' not in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
