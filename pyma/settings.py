518decc5aec8e648d955f5906c1a576492eed47e
PSebastian96 <sebipin96@gmail.com>
2024-01-11 13:32:12 +0000

    allowed hosts synt err

--- pyma/settings.py
+++ pyma/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [, os.environ.get('Herokuhost'),
+ALLOWED_HOSTS = [os.environ.get('Herokuhost'),
                 os.environ.get('gitlocalhost'),
                 os.environ.get('codeanyhost')]
 
