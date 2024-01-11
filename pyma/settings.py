e3ced46202c199fff77edba40d05c73e69e959a8
PSebastian96 <sebipin96@gmail.com>
2024-01-11 15:44:10 +0000

    heroku all host

--- pyma/settings.py
+++ pyma/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get('Herokuhost'),
+ALLOWED_HOSTS = ['pymasnack-pj4-666aee4e185d.herokuapp.com',
                 os.environ.get('gitlocalhost'),]
 
 
